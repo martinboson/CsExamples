5b66a7b60895c049af4f30d1ca9262e3e1a96286
Martin Boson <martin.boson@learnet.se>
2021-05-07 13:53:33 +0200

    C11_Enum

---
+++ C10_Struct/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace C10_Struct
+{
+    // Minneshantering (RAM/HDD)
+    // HEAP - LÅNGSAMMARE MALOC()/FREE()        CLASS
+    // STACK - SNABBARE                         STRUCT   stack overflow
+
+    class ClassExample
+    {
+        public int Nr1 { get; set; }
+
+        public int Nr2 { get; set; }
+
+        public ClassExample(int nr1, int nr2)
+        {
+            Nr1 = nr1;
+            Nr2 = nr2;
+        }
+
+        public void DisplayValues()
+        {
+            Console.WriteLine($"Display Values: {Nr1}, {Nr2}");
+        }
+    }
+    struct StructExample
+    {
+        public int Nr1 { get; set; }
+
+        public int Nr2 { get; set; }
+
+        public StructExample(int nr1, int nr2)
+        {
+            Nr1 = nr1;
+            Nr2 = nr2;
+        }
+
+        public void DisplayValues()
+        {
+            Console.WriteLine($"Display Values: {Nr1}, {Nr2}");
+        }
+    }
+    
+    class Program
+    {
+        private static void ModifyValues(dynamic d)
+        {
+            d.Nr1 += 1;
+            d.Nr2 += 1;
+
+            Console.WriteLine($"Modifying Values: {d.Nr1}, {d.Nr2}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ClassExample - HEAP (GLOBAL)");
+            var classexample = new ClassExample(1,1);
+
+            classexample.DisplayValues();
+            ModifyValues(classexample);
+            classexample.DisplayValues();
+
+            Console.WriteLine("----------------------------------\n");
+
+            Console.WriteLine("StructExample - STACK (LOCAL)");
+            var structexample = new StructExample(1, 1);
+
+            structexample.DisplayValues();
+            ModifyValues(structexample);
+            structexample.DisplayValues();
+            Console.ReadKey();
+
+
+        }
+
+            
+
+    }
+}
---
+++ C11_Enum/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace C11_Enum
+{
+    public enum Level
+    {
+        Low,
+        Normal,
+        High
+    }
+    class Program
+    {
+        enum Status
+        {
+            Pending,
+            Production,
+            Transporting,
+            Delivered
+        }
+        static void Main(string[] args)
+        {
+            var temperature = 32;
+            var _tempLevel = Level.Normal;
+
+            switch (temperature)
+            {
+                case var t when t < 10:
+                    _tempLevel = Level.Low;
+                    
+                    break;
+
+                case var t when t > 30:
+                    _tempLevel = Level.High;
+                    Console.WriteLine("Temp is too high");
+                    break;
+
+                default:
+                    _tempLevel = Level.Normal;
+                    Console.WriteLine("Temp is normal");
+                    break;
+            }
+
+            switch (_tempLevel)
+            {
+                case Level.Low:
+                    Console.WriteLine("Temp is too low");
+                    break;
+                case Level.Normal:
+                    Console.WriteLine("Temp is normal");
+                    break;
+                case Level.High:
+                    Console.WriteLine("Temp is too high");
+                    break;
+            }
+
+            var status = Status.Pending;
+
+            switch (status)
+            {
+                case Status.Pending:
+                    Console.WriteLine("Electronic | information has been received.");
+                    break;
+                case Status.Production:
+                    Console.WriteLine("Your product(s) are being prepared for shipping.");
+                    break;
+                case Status.Transporting:
+                    Console.WriteLine("Your product(s) are in transit.");
+                    break;
+                case Status.Delivered:
+                    Console.WriteLine("Your product(s) has been delivered");
+                    break;
+            }
+        }
+    }
+}
